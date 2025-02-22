208d46adc9b29ddf96ab24d2443063a7f17646e3
MahdiGhezavati <gorganroyal@gmail.com>
2025-02-23 03:12:24 +0330

    created render html files
    add template folder  and html fils to website app and render too views

--- website/views.py
+++ website/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse("this is a personal page and \n<h1>Home Page <h1>")
+    return render(request, "website/Home.html")
 def about_page(request):
-    return HttpResponse("Mahdi ghzavati \n im mahdi and live in iran \n my id in telegram app = @ImahdiI")
+    return render(request, "website/about.html")
