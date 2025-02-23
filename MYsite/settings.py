b6e2cb03cebcf1d891b4187e4b29f0e225901ce3
MahdiGhezavati <gorganroyal@gmail.com>
2025-02-23 04:01:20 +0330

    general templates

--- MYsite/settings.py
+++ MYsite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MYsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templats"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
