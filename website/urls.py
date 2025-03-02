e1e52e739b94221991d8308372333b2afc8ee6e5
MahdiGhezavati <gorganroyal@gmail.com>
2025-03-02 14:41:09 +0330

    change name of page

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from website.views import *
 
+app_name = "website"
 urlpatterns = [
-    path("" , home_page),
-    path("about-me" , about_page)
+    path("" , home_page, name="Index"),
+    path("about-me" , about_page ,name= "About"),
+    path("contact" , contact_page ,name= "Contact")
 ]
