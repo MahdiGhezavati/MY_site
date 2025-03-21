1ab2716ff8d88882e1482dfaddaa8e94addeb3e4
MahdiGhezavati <gorganroyal@gmail.com>
2025-03-22 02:42:19 +0330

    set db in model and admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+class Postadmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = "-"
+    list_display = ["id","title","status","content_view" , "poblished_date","created_date"]
+    list_filter = ["status","created_date"]
+    #ordering = ["created_date"]
+    search_fields = ["title" ,"content"]
+
+admin.site.register(Post , Postadmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    # author 
+    # image
+    # tag
+    # category
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    content_view = models.IntegerField(default = 1)
+    status = models.BooleanField(default=False)
+    poblished_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_date"]
+ 
+    def __str__(self):
+        return f" {self.id} - {self.title} "
+
+
