0b60595f843250e993e937f70b4854cc25746258
MahdiGhezavati <gorganroyal@gmail.com>
2025-03-25 04:10:57 +0330

    edit settings
    change filter by poblished date

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse
 from blog.models import Post
+from datetime import datetime
+# code for get time and use in blog_page
+time_now = datetime.now()
+
 
 
 def blog_page(request):
-    posts = Post.objects.filter(status = 1)
+    posts = Post.objects.filter(poblished_date__lte=time_now)
     context = {"posts":posts}
     return render(request, "blog/blog-home.html" , context)
 
 def single_page(request , pid):
     #post = Post.objects.get(id = pid)
     post = get_object_or_404(Post , pk=pid , status = 1)
+    # create a def for count post views
     def addview(post):
         post.content_view += 1
         return post.save() 
