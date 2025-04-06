ac8ad577f0d0d9e8bb300f52e47c96ac6d02a3b3
MahdiGhezavati <gorganroyal@gmail.com>
2025-04-06 23:41:14 +0330

    create file the name template tag
    use template tag custom with folder in name template-tags and two file in name init and blog tags

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,19 @@
+from django import template
+from blog.models import Post
+from blog.models import category
+
+register = template.Library()
+
+@register.inclusion_tag("blog/sidebar/popular_post.html")
+def latesposts ():
+    posts = Post.objects.filter(status=1).order_by("poblished_date")[:3]
+    return {"posts":posts}  
+
+@register.inclusion_tag("blog/sidebar/category.html")
+def postcategory():
+    posts = Post.objects.filter(status=1)
+    cate = category.objects.all()
+    category_dict = {}
+    for name in cate:
+        category_dict[name]=posts.filter(category=name).count()
+    return {"category":category_dict}
