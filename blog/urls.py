dcdbf15a7e957196bfd38ad618ff19d0ca316b3f
MahdiGhezavati <gorganroyal@gmail.com>
2025-04-06 23:44:46 +0330

    cute side nav
    create popular-post and search and category file in template
    and edit blog-home and blog-views , blog-urls

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ app_name = "blog"
 urlpatterns = [
     path("" , blog_page, name='index'),
     path("<int:pid>" , single_page ,name= 'single'),
+    path('category/<str:cate>' , blog_page , name= 'category'),
+    path("author/<str:author_user>" , blog_page , name= "author" ),
+    path("search/" , search_view , name="search"),
 ]
